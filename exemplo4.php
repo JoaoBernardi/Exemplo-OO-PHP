341f13c5edd17108b4d3f6db830401ccb7757dfd
JoaoBernardi <63605203+JoaoBernardi@users.noreply.github.com>
2020-04-22 14:48:26 -0300

    Update exemplo4.php

--- exemplo4.php
+++ exemplo4.php
@@ -1,21 +1,21 @@
 <?php
-    include 'class/Conta.php';
-    include 'class/ContaCorrente.php';
-    include 'class/ContaPoupanca.php';
-    include 'class/Cliente.php';
+    include 'class/Conta.php'; // incluido uma nova classe Conta
+    include 'class/ContaCorrente.php'; // incluido uma nova classe ContaCorrente
+    include 'class/ContaPoupanca.php'; // incluido uma nova classe ContaPoupanca
+    include 'class/Cliente.php'; // incluido uma nova classe Cliente
 
-    $cc = new ContaCorrente();
-    $poupanca = new ContaPoupanca();
+    $cc = new ContaCorrente(); // nova ContaCorrente
+    $poupanca = new ContaPoupanca(); // nova ContaPoupanca
 
     $contas = [$cc, $poupanca];
 
-    echo 'Saldo inicial Conta Corrente: R$' . $contas[0]->obterSaldo() . '<br>';
-    echo 'Saldo inicial Poupança: R$ ' . $contas[1]->obterSaldo() . '<br>';
+    echo 'Saldo inicial Conta Corrente: R$' . $contas[0]->obterSaldo() . '<br>'; // saldo do valor inicial da Conta Corrente R$
+    echo 'Saldo inicial Poupança: R$ ' . $contas[1]->obterSaldo() . '<br>'; // saldo do valor inicial da Poupança R$
 
-    foreach($contas as $conta)  {
-        $conta->depositar(100);
-        echo 'Depósito de R$ 100. Valor atual: R$ '. 
-            $conta->obterSaldo() . '<br>'; 
+    foreach($contas as $conta)  { 
+        $conta->depositar(100); // deposita-se R$100
+        echo 'Depósito de R$ 100. Valor atual: R$ '. // conclui o valor de R$100
+            $conta->obterSaldo() . '<br>'; // obtem-se o saldo da conta
     }
 
 ?>
