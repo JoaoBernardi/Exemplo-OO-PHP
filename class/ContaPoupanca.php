39c69763f577496ee83d52cdf6fe1f3d2ccb653a
JoaoBernardi <63605203+JoaoBernardi@users.noreply.github.com>
2020-04-22 15:03:44 -0300

    Update ContaPoupanca.php

--- class/ContaPoupanca.php
+++ class/ContaPoupanca.php
@@ -1,6 +1,6 @@
 <?php
 
-    class ContaPoupanca extends Conta {
+    class ContaPoupanca extends Conta { // ContaPoupanca é uma filha de Conta, digamos assim!
 
         function __construct()    {
             parent::__construct();
