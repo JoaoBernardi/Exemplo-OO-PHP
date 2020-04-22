8414acc34009b712583327c418a51532cb81ba30
JoaoBernardi <63605203+JoaoBernardi@users.noreply.github.com>
2020-04-22 14:33:30 -0300

    Update exemplo2.php

--- exemplo2.php
+++ exemplo2.php
@@ -2,16 +2,16 @@
     include 'class/Conta.php';
     include 'class/Cliente.php';
 
-    $conta = new Conta();
-    $cliente = new Cliente('Rafael', '0000000000');
+    $conta = new Conta(); // criada uma nova conta
+    $cliente = new Cliente('Rafael', '0000000000'); // criado um novo cliente Rafael
 
     // Composição
-    $conta->definirTitular($cliente);
+    $conta->definirTitular($cliente); // define-se o titular
 
-    $saldo = $conta->obterSaldo();
-    echo 'Saldo da conta é R$ '. $saldo . '<br>';
+    $saldo = $conta->obterSaldo(); // mostra pra aparecer o Saldo
+    echo 'Saldo da conta é R$ '. $saldo . '<br>'; // saldo da conta estimulado
 
-    $nomeTitular = $conta->obterTitular()->obterNome();
+    $nomeTitular = $conta->obterTitular()->obterNome(); // mostra o nome do titular
     echo 'Nome do Titular: '. $nomeTitular . '<br>';
 
 ?>
